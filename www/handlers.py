5cef751b611d6f1e1bd492c40b2558b43138d1e6
kHRYSTAL <723526676@qq.com>
2016-06-01 01:37:40 +0800

    # [20160601]  add front end depend on uikit

--- www/handlers.py
+++ www/handlers.py
@@ -16,17 +16,25 @@
 import re
 
 import asyncio
+
+import time
+
 from www.coroweb import get
-from www.models import User
+from www.models import User, Blog
 
 
 @get('/')
 @asyncio.coroutine
 def index(request):
-    users = yield from User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
 
 
